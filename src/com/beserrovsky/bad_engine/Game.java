7c440cab67f8d1f0320aa14902eac94bec5425e3
Felipe Beserra <49963588+Beserrovsky@users.noreply.github.com>
2021-05-31 10:16:09 -0300

    Inital commit

---
+++ src/com/beserrovsky/bad_engine/Game.java
@@ -0,0 +1,18 @@
+package com.beserrovsky.bad_engine;
+
+public class Game {
+	
+	public Game() {
+		/*TODO:
+		 * Implement: 
+		 * - frame
+		 * - collision
+		 * - map
+		 * - sound
+		 * */
+	}
+	
+	public static void main(String args[]) {
+		new Game();
+	}
+}
